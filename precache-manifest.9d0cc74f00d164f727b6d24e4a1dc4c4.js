9e14534df51e29c4922fe61bf65c42b2027d79d3
Janno Tabamo <janno@hyperstacksinc.com>
2020-08-12 17:33:50 +0800

    Updates

---
+++ precache-manifest.9d0cc74f00d164f727b6d24e4a1dc4c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b374760558d65520ab3fa4416f40b19",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "8923ea8099b8d80fd6ef",
+    "url": "/resume/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "d523ba1316392347a78c",
+    "url": "/resume/static/js/2.0347b95b.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/resume/static/js/2.0347b95b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8923ea8099b8d80fd6ef",
+    "url": "/resume/static/js/main.e017c3dd.chunk.js"
+  },
+  {
+    "revision": "f838316ee4235b1f15d0",
+    "url": "/resume/static/js/runtime-main.85451859.js"
+  }
+]);
